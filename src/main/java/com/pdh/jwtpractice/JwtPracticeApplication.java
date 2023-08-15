2e8429dc3835fd1cf3f09bf8a609bb0dd2bc9aed
bigbulgogiburger <dohoon321@gmail.com>
2023-08-15 14:11:06 +0900

    Initial commit

---
+++ src/main/java/com/pdh/jwtpractice/JwtPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.pdh.jwtpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtPracticeApplication.class, args);
+	}
+
+}
